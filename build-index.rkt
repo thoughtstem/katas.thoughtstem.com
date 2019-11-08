2f623ae3355972be43079fb3af34c4cf0c0dce09
sonnynajar <sonny.najar@gmail.com>
2019-11-08 13:12:19 -0800

    Update build-index.rkt

--- build-index.rkt
+++ build-index.rkt
@@ -13,4 +13,5 @@
           (collection-link 'K2 "Winter K-2nd" #:link-path "K2/index.html")
           (collection-link 'GE "2D Games (Fall 2019)" #:link-path "GE/index.html")     
           (collection-link 'VR "3D Games" #:link-path "VR/index.html")
+          (collection-link 'MISC "MISC Languages" #:link-path "MISC/index.html")
           )))))
