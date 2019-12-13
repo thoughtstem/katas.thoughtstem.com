8ac66fb586833ed1a956b84176798707b96994a4
sonnynajar <sonny.najar@gmail.com>
2019-12-13 08:01:26 -0800

    added ME

--- build-index.rkt
+++ build-index.rkt
@@ -11,8 +11,9 @@
         "Kata Collections"
         (~a 
           (collection-link 'COACH "Coach Resources" #:link-path "COACH/index.html")
-          (collection-link 'K2 "Winter K-2nd" #:link-path "K2/index.html")
-          (collection-link 'GE "2D Games (Fall 2019)" #:link-path "GE/index.html")     
+          (collection-link 'K2 "K-2nd Games" #:link-path "K2/index.html")
+          (collection-link 'GE "2D Games (GE)" #:link-path "GE/index.html")
+          (collection-link 'ME "2D Games (ME)" #:link-path "ME/index.html")     
           (collection-link 'VR "3D Games" #:link-path "VR/index.html")
           (collection-link 'MISC "Misc Languages" #:link-path "MISC/index.html")
           )))))
